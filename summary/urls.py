5a533314bfeec9f0732b22afd1bff090abe248fd
bjoshikamal652-code <bjoshikamal652@gmail.com>
2026-01-09 19:22:45 +0530

    Update urls.py

--- summary/urls.py
+++ summary/urls.py
@@ -2,7 +2,7 @@ from .views import front, sign_in, home, signout
 from django.urls import path
 
 urlpatterns = [path("", front, name='front'),
-path('sign_in/', sign_in),
-path('home/', home),
-path('signout/', signout),
+path('sign_in/', sign_in, name='sign_in'),
+path('home/', home, name='home'),
+path('signout/', signout, name='signout'),
 ]
